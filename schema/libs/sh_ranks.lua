e711d1361365b0b22ccc26b74c60ccf7215355e0
eon (bloodycop) <minerva.servers.eon@gmail.com>
2024-12-08 18:55:02 +0200

    Update sh_ranks.lua

--- schema/libs/sh_ranks.lua
+++ schema/libs/sh_ranks.lua
@@ -1,7 +1,3 @@
-if (SERVER) then
-	util.AddNetworkString("ixRankUpdate")
-end
-
 ix.rank = ix.rank or {}
 ix.rank.list = {}
 
@@ -10,151 +6,262 @@ ix.char.RegisterVar("rank", {
 })
 
 local charMeta = ix.meta.character
-
 function ix.rank.LoadFromDir(directory)
-	for _, v in pairs(file.Find(directory.."/*.lua", "LUA")) do
-		local niceName = v:sub(4, -5)
-		local index = #ix.rank.list + 1
-		local halt
+    for k, v in ipairs(file.Find(directory.."/*.lua", "LUA")) do
+        local niceName = v:sub(4, -5)
+        -- Determine a numeric identifier for this rank.
+        local index = #ix.rank.list + 1
+        local halt
 
-		for _, v2 in ipairs(ix.rank.list) do
-			if (v2.uniqueID == niceName) then
-				halt = true
-			end
-		end
+        for _, v2 in ipairs(ix.rank.list) do
+            if (v2.uniqueID == niceName) then
+                halt = true
 
-		if (halt == true) then
-			continue
-		end
+                break
+            end
+        end
 
-		RANK = {index = index, uniqueID = niceName}
-			RANK.name = "Unknown"
-			RANK.description = "No description available."
-			RANK.limit = 0
+        if (halt == true) then continue end
 
-			if (PLUGIN) then
-				RANK.plugin = PLUGIN.uniqueID
-			end
+        -- Set up a global table so the file has access to the rank table.
+        RANK = {index = index, uniqueID = niceName}
+            RANK.name = "Unknown"
+            RANK.description = "No description available."
+            RANK.limit = 0
 
-			ix.util.Include(directory.."/"..v, "shared")
+            -- For future use with plugins.
+            if (PLUGIN) then
+                RANK.plugin = PLUGIN.uniqueID
+            end
 
-			if (!RANK.faction or !team.Valid(RANK.faction)) then
-				ErrorNoHalt("Rank '"..niceName.."' does not have a valid faction!\n")
-				RANK = nil
+            ix.util.Include(directory.."/"..v, "shared")
 
-				continue
-			end
+            -- Why have a rank without a faction?
+            if (!RANK.faction or !team.Valid(RANK.faction)) then
+                ErrorNoHalt("Rank '"..niceName.."' does not have a valid faction!\n")
+                RANK = nil
 
-			if (!RANK.CanSwitchTo) then
-				RANK.CanSwitchTo = function(client)
-					return true
-				end
-			end
+                continue
+            end
 
-			ix.rank.list[index] = RANK
-		RANK = nil
-	end
+            -- Allow rank to be joinable by default.
+            if (!RANK.CanSwitchTo) then
+                RANK.CanSwitchTo = function(client)
+                    return true
+                end
+            end
+
+            ix.rank.list[index] = RANK
+        RANK = nil
+    end
 end
 
+--- Determines if a player is allowed to join a specific rank.
+-- @realm shared
+-- @player client Player to check
+-- @number rank Index of the rank
+-- @treturn bool Whether or not the player can switch to the rank
 function ix.rank.CanSwitchTo(client, rank)
-	local info = ix.rank.list[rank]
+    -- Get the rank table by its numeric identifier.
+    local info = ix.rank.list[rank]
 
-	if (!info) then
-		return false, "no info"
-	end
+    -- See if the rank exists.
+    if (!info) then
+        return false, "no info"
+    end
 
-	if (client:Team() != info.faction) then
-		return false, "not correct team"
-	end
+    -- If the player's faction matches the rank's faction.
+    if (client:Team() != info.faction) then
+        return false, "not correct team"
+    end
 
-	if (client:GetCharacter():GetRank() == rank) then
-		return false, "same class request"
-	end
+    if (info.limit > 0) then
+        if (#ix.rank.GetPlayers(info.index) >= info.limit) then
+            return false, "rank is full"
+        end
+    end
 
-	if (info.limit > 0) then
-		if (#ix.rank.GetPlayers(info.index) >= info.limit) then
-			return false, "rank is full"
-		end
-	end
+    local canJoin, reason = hook.Run("CanPlayerJoinRank", client, rank, info)
+    if (canJoin == false) then
+        return false, reason
+    end
 
-	if (hook.Run("CanPlayerJoinRank", client, rank, info) == false) then
-		return false
-	end
-
-	return info:CanSwitchTo(client)
+    -- See if the rank allows the player to join it.
+    return info:CanSwitchTo(client)
 end
 
+--- Retrieves a rank table.
+-- @realm shared
+-- @number identifier Index of the rank
+-- @treturn table Rank table
 function ix.rank.Get(identifier)
-	return ix.rank.list[identifier]
+    for _, v in ipairs(ix.rank.list) do
+        if ( ix.util.StringMatches(v.uniqueID, tostring(identifier)) or ix.util.StringMatches(v.name, tostring(identifier)) or v.index == identifier ) then
+            return v
+        end
+    end
+
+    return nil
 end
 
+--- Retrieves the players in a rank
+-- @realm shared
+-- @number rank Index of the rank
+-- @treturn table Table of players in the rank
 function ix.rank.GetPlayers(rank)
-	local players = {}
+    local players = {}
 
-	for _, v in player.Iterator() do
-		local char = v:GetCharacter()
+    for _, v in player.Iterator() do
+        if ( !IsValid(v) ) then continue end
 
-		if (char and char:GetRank() == rank) then
-			table.insert(players, v)
-		end
-	end
+        local char = v:GetCharacter()
+        if (char and char:GetRank() == rank) then
+            players[#players + 1] = v
+        end
+    end
 
-	return players
+    return players
+end
+
+--- Retrieves the character's rank data (name, description, etc).
+-- @realm shared
+function charMeta:GetRankData()
+    if ( !self ) then return nil end
+
+    return ix.rank.Get(self:GetRank()) or nil
 end
 
 if (SERVER) then
-	function charMeta:JoinRank(rank)
-		if (!class) then
-			self:KickRank()
-			return false
-		end
+    --- Character rank methods
+    -- @classmod Character
 
-		local oldRank = self:GetRank()
-		local client = self:GetPlayer()
+    --- Makes this character join a rank. This automatically calls `KickRank` for you.
+    -- @realm server
+    -- @number rank Index of the rank to join
+    -- @treturn bool Whether or not the character has successfully joined the rank
+    function charMeta:JoinRank(rank)
+        if (!rank) then
+            self:KickRank()
+            return false
+        end
 
-		if (ix.rank.CanSwitchTo(client, class)) then
-			self:SetRank(class)
-			hook.Run("PlayerJoinedRank", client, rank, oldRank)
+        local oldRank = self:GetRank()
+        local client = self:GetPlayer()
 
-			return true
-		end
+        if (ix.rank.CanSwitchTo(client, rank)) then
+            self:SetRank(rank)
+            hook.Run("PlayerJoinedRank", client, rank, oldRank)
 
-		return false
-	end
+            return true
+        end
 
-	function charMeta:KickRank()
-		local client = self:GetPlayer()
-		if (!client) then return end
+        return false
+    end
 
-		local goRank
+    --- Kicks this character out of the rank they are currently in.
+    -- @realm server
+    -- @usage character:KickRank()
+    function charMeta:KickRank()
+        local client = self:GetPlayer()
+        if ( !IsValid(client) ) then return end
 
-		for k, v in pairs(ix.rank.list) do
-			if (v.faction == client:Team() and v.isDefault) then
-				goRank = k
+        local goRank
 
-				break
-			end
-		end
+        for k, v in ipairs(ix.rank.list) do
+            if (v.faction == client:Team() and v.isDefault) then
+                goRank = k
 
-		self:JoinRank(goRank)
+                break
+            end
+        end
 
-		hook.Run("PlayerJoinedRank", client, goRank)
-	end
+        if ( !goRank ) then
+            self:SetRank(nil)
+            return
+        end
 
-	function GAMEMODE:PlayerJoinedRank(client, rank, oldRank)
-		local info = ix.rank.list[rank]
-		local info2 = ix.rank.list[oldRank]
+        self:JoinRank(goRank)
 
-		if (info.OnSet) then
-			info:OnSet(client)
-		end
+        hook.Run("PlayerJoinedRank", client, goRank)
+    end
 
-		if (info2 and info2.OnLeave) then
-			info2:OnLeave(client)
-		end
+    --- Promotes the character to the next rank in order.
+    -- @realm server
+    -- @treturn bool Whether or not the character has been promoted
+    -- @usage character:RankPromote() -- returns true if the character has been promoted
+    function charMeta:RankPromote()
+        if ( !self ) then return false end
 
-		net.Start("ixRankUpdate")
-			net.WritePlayer(client)
-		net.Broadcast()
-	end
+        local ply = self:GetPlayer()
+        if ( !IsValid(ply) ) then return false end
+
+        local rank = self:GetRank()
+        if ( !rank ) then return false end
+
+        local rankData = ix.rank.Get(rank)
+        if ( !rankData ) then return false end
+
+        if ( !rankData.sortOrder ) then
+            ErrorNoHaltWithStack("Rank \"" .. rankData.name .. "\" does not have a sortOrder value!\n")
+            return false
+        end
+
+        local nextRank = rankData.sortOrder + 1
+
+        for k, v in ipairs(ix.rank.list) do
+            if ( v.sortOrder == nextRank and v.faction == self:GetFaction() ) then
+                self:SetRank(k)
+                hook.Run("PlayerJoinedRank", ply, k, rank)
+                break
+            end
+        end
+    end
+
+    --- Demotes the character to the previous rank in order.
+    -- @realm server
+    -- @treturn bool Whether or not the character has been demoted
+    -- @usage character:RankDemote() -- returns true if the character has been demoted
+    function charMeta:RankDemote()
+        if ( !self ) then return end
+
+        local ply = self:GetPlayer()
+        if ( !IsValid(ply) ) then return end
+
+        local rank = self:GetRank()
+        if ( !rank ) then return end
+
+        local rankData = ix.rank.Get(rank)
+        if ( !rankData ) then return end
+
+        if ( !rankData.sortOrder ) then
+            ErrorNoHaltWithStack("Rank \"" .. rankData.name .. "\" does not have a sortOrder value!\n")
+            return
+        end
+
+        local nextRank = rankData.sortOrder - 1
+
+        for k, v in ipairs(ix.rank.list) do
+            if ( v.sortOrder == nextRank and v.faction == self:GetFaction() ) then
+                self:SetRank(k)
+                hook.Run("PlayerJoinedRank", ply, k, rank)
+
+                return true
+            end
+        end
+
+        return false
+    end
+
+    function GM:PlayerJoinedRank(client, rank, oldRank)
+        local info = ix.rank.list[rank]
+        local info2 = ix.rank.list[oldRank]
+
+        if (info and info.OnSet) then
+            info:OnSet(client)
+        end
+
+        if (info2 and info2.OnLeave) then
+            info2:OnLeave(client)
+        end
+    end
 end
